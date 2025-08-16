477833b320a232102cbab96b7eae320489a2ed7c
Masoud Mohammadi <masoudmhmdi86@gmail.com>
2025-08-16 20:07:57 +0330

    chore:start avl tree

---
+++ LeetCodes/AVLTree/AVLTree.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCodes.AVLTree;
+
+
+public class Node
+{
+
+  public Node? Left { get; set; }
+  public Node? Right { get; set; }
+  public int Value { get; set; }
+  public int Height { get; set; } = 0;
+  public Node(int val)
+  {
+    this.Value = val;
+  }
+}
+
+public class AVLTree
+{
+  private Node _root;
+
+  public AVLTree(Node root)
+  {
+    _root = root;
+  }
+
+  // left,right
+  public (int,int) Insert(Node root, int value)
+  {
+    
+    if (value > root.Value)
+    {
+      if (root.Right == null)
+      {
+        root.Right = new Node(value);
+      }
+      else
+      {
+        Insert(root.Right, value);
+      }
+    }
+
+    if (value < root.Value)
+    {
+      if (root.Left == null)
+      {
+        root.Left = new Node(value);
+      }
+      else
+      {
+        Insert(root.Left, value);
+      }           
+    }
+
+
+    return (0, 0);
+
+  }
+  
+  
+  
+  
+}
--- LeetCodes/Program.cs
+++ LeetCodes/Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public class Program()
+{
+
+    public static void Main()
+    {
+        var data = new DTO()
+        {
+            data = "noModified"
+        };
+
+
+        Program.Modifier(data);
+        
+        Console.WriteLine("Hello World!");
+        
+    }
+
+    public static void Modifier(DTO dto)
+    {
+        dto = null;
+    }
+}
+
+
+public class DTO
+{
+    public string data { get; set; }
+}
